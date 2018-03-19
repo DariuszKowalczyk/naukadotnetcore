4a36d6dc4e75b49e1e411f6d0a973cbaee9f14f1
Dariusz Kowalczyk <dariusztomaszkowalczyk@gmail.com>
2018-03-19 22:35:48 +0100

    Dodanie testu ShouldReturnListOfUsers() bez implementacji

--- WebApp/XUnitTestProject1/UnitTest1.cs
+++ WebApp/XUnitTestProject1/UnitTest1.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace XUnitTestProject1
@@ -6,9 +9,87 @@ namespace XUnitTestProject1
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ShouldReturnListOfUsers()
         {
+            // Arrange
+            var users = new List<User>()
+            {
+                new User()
+                {
+                    Username = "konto1",
+                    Password = "konto1",
+                    Email = "konto1@api.pl"
+                },
+                new User()
+                {
+                    Username = "konto2",
+                    Password = "konto2",
+                    Email = "konto2@api.pl"
+                },
+            };
+
+            var userRepository = new Mock<IRepository<User>>();
+            userRepository.Setup(x => x.GetAll()).Returns(users);
+            var userService = new UserService(userRepository.Object);
+            var userController = new UserController(userService);
+
+            // Act
+            var result = userController.Get();
+
+            // Assert
+            Assert.Equal(users, result);
 
         }
+    }
+
+    public class UserController : Controller
+    {
+        private UserService userService;
+
+        public UserController(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public List<User> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IUserService
+    {
+        List<User> GetUser();
+    }
+
+    public class UserService : IUserService
+    {
+        private IRepository<User> _userRepository;
+
+        public UserService(IRepository<User> userRepository)
+        {
+            this._userRepository = userRepository;
+        }
+
+        public List<User> GetUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IRepository<T> where T : Entity
+    {
+        List<T> GetAll();
+    }
+
+    public class User : Entity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+    public class Entity
+    {
+        public long Id { get; set; }
     }
 }
